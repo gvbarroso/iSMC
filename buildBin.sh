830db5b821d319000574d22a66fba9e37ab4d761
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2026-02-11 10:53:17 +0100

    Updated bin script.

--- buildBin.sh
+++ buildBin.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 arch=`uname -m`
-version=0.0.25-1
+version=1.0.0-1
 
 strip src/ismc
 strip src/ismc_mapper
-tar cvzf ismc-${arch}-bin-static-${version}.tar.gz src/ismc src/ismc_mapper
+strip src/ismc_mapper2
+tar cvzf ismc-${arch}-bin-static-${version}.tar.gz src/ismc src/ismc_mapper src/ismc_mapper2
 
